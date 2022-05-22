c30d2491e10e7085f239b352fe3b838f1b1b262d
Varanda <m@varanda.ca>
2022-05-22 15:33:19 -0400

    initial design for pedal screen.

---
+++ design/SquareLine_Project_events.py
@@ -0,0 +1,20 @@
+
+def onAbout(event_struct):
+    return
+
+
+def onWave(event_struct):
+    return
+
+
+def onPedalView(event_struct):
+    return
+
+
+def onWaveView(event_struct):
+    return
+
+
+def onAboutView(event_struct):
+    return
+
