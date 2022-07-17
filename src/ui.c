4a9d526b4b65d8014e53e49ee431a0aa4f1d490f
Varanda <m@varanda.ca>
2022-07-17 10:22:22 -0400

    added test to hunt the crash.

--- src/ui.c
+++ src/ui.c
@@ -23,6 +23,11 @@
 #include "pedal.h"
 #include "generic_props_ctl.h"
 
+#ifdef PLATFORM_ARM_QEMU
+extern lv_indev_t * touch_indev;
+lv_indev_t * global_indev;
+#endif
+
 #define SHOW_LOGO
 
 #define USE_DISPLAY_BACKGROUND
@@ -1188,8 +1193,52 @@ void ui_Dialog_init(void)
 
 }
 
+//---------------- test -----------------
+static void event_handler(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        LV_LOG_USER("Clicked");
+    }
+    else if(code == LV_EVENT_VALUE_CHANGED) {
+        LV_LOG_USER("Toggled");
+    }
+}
+static void test(void)
+{
+    lv_obj_t * label;
+
+    lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
+    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+
+    label = lv_label_create(btn1);
+    lv_label_set_text(label, "Button");
+    lv_obj_center(label);
+
+    lv_obj_t * btn2 = lv_btn_create(lv_scr_act());
+    lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
+    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+    lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+
+    label = lv_label_create(btn2);
+    lv_label_set_text(label, "Toggle");
+    lv_obj_center(label);
+}
+//------------------------------------------
+
 void ui_init(void)
 {
+#if 0
+    test();
+#else
+
+
+#ifdef PLATFORM_ARM_QEMU
+    global_indev = touch_indev;
+#endif
     memset(boards, 0, sizeof(boards));
     memset(available_pedals, 0, sizeof(available_pedals));
 
@@ -1236,6 +1285,7 @@ void ui_init(void)
     // just test
     ui_Dialog_init();
     dialog_hide();
+#endif
 }
 
 
